b53db5c499bed04ffa711b8c7a867c8697dffc60
Zakarya-Mks <zakaaryaaa@gmail.com>
2020-12-17 18:21:51 +0100

    feat: implement tow player game logic, get the winner or end the round with a draw.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 const DomElement = (function () {
   const menuBtn = document.querySelector('.burger_menu_btn ');
-  const marks = document.querySelector('.marks');
   const rightSection = document.querySelector('.right_section');
   const leftSection = document.querySelector('.left_section ');
-  const mark_x = document.querySelector('.x');
-  const mark_o = document.querySelector('.o');
   const board = document.querySelector('.game_board');
   const boardChildrenArr = Array.from(board.children);
   const gameStartingDialog = document.querySelector('.game_start_options');
@@ -21,14 +18,12 @@ const DomElement = (function () {
   const GSD_playerVsComputerOption_inputs = document.querySelectorAll(
     '.player_vs_ai input'
   );
+  const roundEnded_dialog = document.querySelector('.roundEnded');
 
   return {
     menuBtn,
-    marks,
     rightSection,
     leftSection,
-    mark_x,
-    mark_o,
     board,
     boardChildrenArr,
     gameStartingDialog,
@@ -40,70 +35,140 @@ const DomElement = (function () {
     leftSectionLog,
     turns,
     leftSectionRound,
+    roundEnded_dialog,
   };
 })();
 
-const Player = function (Name, Mark, WillPlayNext) {
+const Player = function (Name, Mark) {
   const name = Name;
   const mark = Mark;
-  let willPlayNext = WillPlayNext;
-  const wonRound = [];
-  const lostRounds = [];
-
-  let score = 0;
+  const score = {
+    won: 0,
+    lost: 0,
+  };
 
   const getName = () => {
     return name;
   };
 
-  const play = () => {
-    if (willPlayNext) {
-      uiController.leftSectionController.clearPlayerMark();
-      [...DomElement.turns.children].forEach((player) => {
-        //remove selection for all player and then add it to the one playing next
-        player.classList.remove('active');
-
-        if ([...player.children][1].textContent === name) {
-          player.classList.add('active');
-        }
-      });
-      DomElement[`mark_${mark}`].classList.add('selected');
-    }
-
-    willPlayNext = !willPlayNext;
+  const getMark = () => {
+    return mark;
   };
 
-  return { getName, play };
+  const getScore = () => {
+    return [score.won, score.lost];
+  };
+
+  const won = function (wonOrNot) {
+    if (wonOrNot) {
+      score.won++;
+    } else {
+      score.lost++;
+    }
+  };
+
+  const select = () => {
+    uiController.rightSectionController.removeActivePlayerMark(name);
+  };
+
+  return { getName, getMark, getScore, select, won };
 };
 
 const GameBoard = (function () {
   const _gameBoard = new Array(9);
 
-  const _checkForEmptySpot = (index) => {
+  const _checkIfCellEmpty = (index) => {
     return !_gameBoard[index] ? true : false;
   };
 
-  const add = (mark, index) => {
-    if (index < 9 && ['x', 'o'].includes(mark)) {
-      if (_checkForEmptySpot(index)) {
-        _gameBoard[index] = mark;
+  const _checkForRemainingEmptyCells = () => {
+    if (!_gameBoard.includes(undefined)) {
+      _endTheGame();
+      GameLogic.endRound(undefined);
+    }
+  };
 
-        uiController.rightSectionController.renderGameBoard(_gameBoard);
+  const _endTheGame = () => {
+    DomElement.board.removeEventListener('click', DomListener.gridCellClick);
+    uiController.rightSectionController.removeActivePlayerMark();
+  };
 
-        GameLogic.nextPlayer();
-      } else {
-        uiController.rightSectionController.gameBoardError(index);
+  const _checkForWinner = function () {
+    const winningFormulas = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+    let x_arr = [];
+    let o_arr = [];
+
+    //when the first player enter the 3rd mark start checking for a winner
+    const resultArray = _gameBoard.filter((item) => item != undefined);
+    if (resultArray.length >= 5) {
+      //get indexes of each mark in separate array
+      _gameBoard.forEach((item, index) => {
+        if (item == 'x') {
+          x_arr.push(index + 1);
+        } else if (item == 'o') {
+          o_arr.push(index + 1);
+        }
+      });
+
+      //compare with the winning formula
+      for (let index = 0; index < winningFormulas.length; index++) {
+        const formula = winningFormulas[index];
+        let x_result = formula.every((item) => x_arr.includes(item));
+        let o_result = formula.every((item) => o_arr.includes(item));
+
+        if (x_result || o_result) {
+          _endTheGame();
+          return true;
+        }
       }
     }
   };
 
-  return { add };
+  const resetBoard = function () {
+    _gameBoard.length = 0;
+    _gameBoard.length = 9;
+    uiController.rightSectionController.cleanGameBoard();
+  };
+
+  const fillCell = (index) => {
+    let currPlayer;
+
+    if (_checkIfCellEmpty(index)) {
+      currPlayer = GameLogic.whoIsNext();
+      const playerMark = currPlayer.getMark();
+
+      _gameBoard[index] = playerMark;
+
+      uiController.rightSectionController.renderGameBoard(_gameBoard);
+    } else {
+      uiController.rightSectionController.gameBoardError(index);
+    }
+
+    if (_checkForWinner()) {
+      GameLogic.endRound(currPlayer);
+    } else {
+      _checkForRemainingEmptyCells();
+    }
+  };
+
+  return { fillCell, resetBoard };
 })();
 
 const GameLogic = (function () {
-  let round = 1;
   let player1, player2;
-  const setNewGame = function (gameType) {
+  let whoIsNextIndex = 0;
+  let round = 1;
+
+  const startNewGame = function (gameType) {
     if (gameType === 'twoPlayers') {
       const player_1 = DomElement.GSD_twoPlayerOption_inputs[0].value
         ? DomElement.GSD_twoPlayerOption_inputs[0].value.trim()
@@ -112,15 +177,15 @@ const GameLogic = (function () {
         ? DomElement.GSD_twoPlayerOption_inputs[1].value.trim()
         : 'Player 2';
 
-      player1 = new Player(player_1, 'x', true);
-      player2 = new Player(player_2, 'o', false);
+      player1 = new Player(player_1, 'x');
+      player2 = new Player(player_2, 'o');
     } else if (gameType === 'onePlayer') {
       const player_1 = DomElement.GSD_playerVsComputerOption_inputs[0].value
         ? DomElement.GSD_playerVsComputerOption_inputs[0].value
         : 'Player 1';
       const player_2 = 'computer';
-      player1 = new Player(player_1, 'x', true);
-      player2 = new Player(player_2, 'o', false);
+      player1 = new Player(player_1, 'x');
+      player2 = new Player(player_2, 'o');
     }
 
     uiController.leftSectionController.updateLogPlayerNames(
@@ -132,15 +197,47 @@ const GameLogic = (function () {
       player2.getName()
     );
     uiController.leftSectionController.updateLogRound(round);
-    nextPlayer();
+    player1.select();
   };
 
-  const nextPlayer = function () {
-    player1.play();
-    player2.play();
+  const whoIsNext = function () {
+    const player = [player1, player2][whoIsNextIndex];
+    whoIsNextIndex = whoIsNextIndex === 0 ? 1 : 0;
+    [player1, player2][whoIsNextIndex].select();
+    return player;
   };
 
-  return { setNewGame, nextPlayer };
+  const newRound = function () {
+    DomElement.roundEnded_dialog.style.display = 'none';
+    uiController.leftSectionController.updateLogRound(round);
+    GameBoard.resetBoard();
+    DomElement.board.addEventListener('click', DomListener.gridCellClick);
+    whoIsNextIndex = 0;
+    player1.select();
+  };
+
+  const endRound = function (winner) {
+    if (winner) {
+      if (winner === player1) {
+        player1.won(true);
+        player2.won(false);
+      } else {
+        player1.won(false);
+        player2.won(true);
+      }
+
+      uiController.leftSectionController.updateScore(
+        player1.getScore()[0],
+        player2.getScore()[0]
+      );
+    }
+
+    round++;
+    DomElement.roundEnded_dialog.style.display = 'flex';
+    uiController.rightSectionController.displayRoundEndDialog(winner);
+  };
+
+  return { startNewGame, endRound, newRound, whoIsNext };
 })();
 
 const uiController = (function () {
@@ -184,7 +281,7 @@ const uiController = (function () {
           DomElement.gameStartingDialog.style.display = 'none';
           DomElement.rightSection.style.display = 'flex';
           DomElement.leftSection.style.display = 'flex';
-          GameLogic.setNewGame(gameType);
+          GameLogic.startNewGame(gameType);
         } else {
           setTimeout(() => {
             target.classList.remove('shake-horizontal');
@@ -207,22 +304,20 @@ const uiController = (function () {
       if (DomElement.menuBtn.classList.contains('show')) {
         DomElement.leftSection.style.transform = 'translateX(0)';
         DomElement.menuBtn.style.right = '0';
+        DomElement.menuBtn.classList.remove('menu-btn-for-md');
         DomElement.rightSection.classList.remove('center');
         setTimeout(() => {
           DomElement.menuBtn.classList.add('close');
         }, 250);
       } else {
         DomElement.leftSection.style.transform = 'translateX(-100%)';
-        DomElement.menuBtn.style.right = '-5rem';
+        DomElement.menuBtn.style.right = '-3.5rem';
+        DomElement.menuBtn.classList.add('menu-btn-for-md');
         DomElement.rightSection.classList.add('center');
         setTimeout(() => {
           DomElement.menuBtn.classList.remove('close');
         }, 250);
       }
-    },
-    clearPlayerMark: function () {
-      DomElement.mark_o.classList.remove('selected');
-      DomElement.mark_x.classList.remove('selected');
     },
     updateLogPlayerNames: function (player1, player2) {
       let counter = 0;
@@ -234,12 +329,28 @@ const uiController = (function () {
         }
       });
     },
+    updateScore: function (player1Score, player2Score) {
+      let counter = 0;
+      [...DomElement.leftSectionLog.children].forEach((child, index) => {
+        if (child.nodeName === 'DIV') {
+          [...child.children][1].textContent = [player1Score, player2Score][
+            counter
+          ];
+          counter++;
+        }
+      });
+    },
     updateLogRound: function (round) {
       [...DomElement.leftSectionRound.children][1].textContent = round;
     },
   };
 
   const rightSectionController = {
+    cleanGameBoard: function () {
+      DomElement.boardChildrenArr.forEach((element) => {
+        element.classList.remove('js-x', 'js-o');
+      });
+    },
     renderGameBoard: function (dataArray) {
       DomElement.boardChildrenArr.forEach((element, index) => {
         if (dataArray[index]) {
@@ -268,6 +379,28 @@ const uiController = (function () {
         );
       }
     },
+    removeActivePlayerMark: (exception) => {
+      [...DomElement.turns.children].forEach((player) => {
+        player.classList.remove('active');
+
+        if ([...player.children][1].textContent === exception) {
+          player.classList.add('active');
+        }
+      });
+    },
+
+    displayRoundEndDialog: function (winner) {
+      DomElement.roundEnded_dialog.style.display = 'flex';
+      [...[...DomElement.roundEnded_dialog.children][0].children].forEach(
+        (item) => {
+          if (item.tagName === 'H1') {
+            item.textContent = winner
+              ? `${winner.getName()} Won this Round`
+              : 'DRAW';
+          }
+        }
+      );
+    },
   };
 
   return {
@@ -277,30 +410,58 @@ const uiController = (function () {
   };
 })();
 
-const DomEventListeners = (function () {
-  DomElement.gameStartingDialog.addEventListener('click', (e) => {
+const DomListener = (function () {
+  const startExitGame = (e) => {
     e.stopPropagation();
     if (e.target.closest('.card')) {
       uiController.startingDialogController.toggleGameMode(e.target);
     } else if (e.target.closest('.controls')) {
       uiController.startingDialogController.startOrExitGame(e.target);
     }
-  });
+  };
 
-  // show/hide the side menu and change the menu icon accordingly
-  DomElement.menuBtn.addEventListener('click', (e) => {
+  const showHideMenu = (e) => {
     e.stopPropagation();
     uiController.leftSectionController.showHideMenu();
-  });
+  };
+
+  const gridCellClick = (e) => {
+    if (e.target.closest('.board_column')) {
+      e.stopPropagation();
+      const clicked_column = e.target.closest('.board_column');
+      const cellIndex = Array.from(DomElement.board.children).indexOf(
+        clicked_column
+      );
+
+      GameBoard.fillCell(cellIndex);
+    }
+  };
+
+  const roundEnded_dialogClick = function (e) {
+    if (e.target.id === 'newRound') {
+      GameLogic.newRound();
+    } else if (e.target.id === 'newGame') {
+      location.reload();
+    }
+  };
+
+  return { gridCellClick, showHideMenu, startExitGame, roundEnded_dialogClick };
+})();
+
+const DomEvent = (function () {
+  DomElement.gameStartingDialog.addEventListener(
+    'click',
+    DomListener.startExitGame
+  );
+
+  // show/hide the side menu and change the menu icon accordingly
+  DomElement.menuBtn.addEventListener('click', DomListener.showHideMenu);
 
   // fill board column with players mark when clicked
-  DomElement.board.addEventListener('click', (e) => {
-    if (e.target.closest('.board_column')) {
-      const mark = document.querySelector('.marks .selected').classList[0];
-      const clicked_column = e.target.closest('.board_column');
-      const gameBoard = Array.from(DomElement.board.children);
+  DomElement.board.addEventListener('click', DomListener.gridCellClick);
 
-      GameBoard.add(mark, gameBoard.indexOf(clicked_column));
-    }
-  });
+  DomElement.roundEnded_dialog.addEventListener(
+    'click',
+    DomListener.roundEnded_dialogClick
+  );
 })();
